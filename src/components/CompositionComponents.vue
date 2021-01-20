5e2cc3b685380182d701bb9e4855bfc453c1b113
mamoru igamoto <stream99990@yahoo.co.jp>
2021-01-20 21:55:17 +0900

    add: Learn setup

---
+++ src/components/CompositionComponents.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>CompositionComponents</h2>
+    <p>user: {{ user }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    console.log(props); // { user: '' }
+
+    return {}; // anything returned here will be available for the rest of the component
+  },
+};
+</script>
+
+<style></style>
